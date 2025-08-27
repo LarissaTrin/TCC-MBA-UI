e1e5756294214db42790bb326e71d94eecdf6b0c
Lua <lala.trindade.2008@gmail.com>
2025-08-27 20:22:34 -0300

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login';
+import { HomeComponent } from './components/home/home';
+import { RegisterComponent } from './components/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: 'login' },
+];
---
+++ src/app/shared/widgets/textfield/textfield.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textfield',
+  imports: [],
+  templateUrl: './textfield.html',
+  styleUrl: './textfield.css'
+})
+export class Textfield {
+
+}
